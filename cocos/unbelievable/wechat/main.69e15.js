61f81bdf8aa17d0177b75807f478b982d878ae66
JovenPan <joven@macbook-pro-15>
2024-10-23 23:21:24 +0800

    fix: 智商在哪里

--- cocos/unbelievable/wechat/main.69e15.js
+++ cocos/unbelievable/wechat/main.69e15.js
@@ -135,7 +135,7 @@ window.boot = function () {
         BK.Script.loadlib();
     }
     else {
-        var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.8cbb4.js';
+        var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.8ade4.js';
         if (jsList) {
             jsList = jsList.map(function (x) {
                 return 'src/' + x;
@@ -161,7 +161,7 @@ window.boot = function () {
     cc.game.run(option, onStart);
 };
 
-// main.40393.js is qqplay and jsb platform entry file, so we must leave platform init code here
+// main.69e15.js is qqplay and jsb platform entry file, so we must leave platform init code here
 if (false) {
     BK.Script.loadlib('GameRes://src/settings.js');
     BK.Script.loadlib();
