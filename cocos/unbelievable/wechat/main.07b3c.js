90600700a3e902ad797185c19b07eb405ce19e3d
JovenPan <joven@macbook-pro-15>
2024-10-31 01:25:32 +0800

    feat: 调试智商在哪儿devicemotion

--- cocos/unbelievable/wechat/main.07b3c.js
+++ cocos/unbelievable/wechat/main.07b3c.js
@@ -135,7 +135,7 @@ window.boot = function () {
         BK.Script.loadlib();
     }
     else {
-        var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.a3161.js';
+        var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.868e9.js';
         if (jsList) {
             jsList = jsList.map(function (x) {
                 return 'src/' + x;
@@ -161,7 +161,7 @@ window.boot = function () {
     cc.game.run(option, onStart);
 };
 
-// main.c9cc6.js is qqplay and jsb platform entry file, so we must leave platform init code here
+// main.07b3c.js is qqplay and jsb platform entry file, so we must leave platform init code here
 if (false) {
     BK.Script.loadlib('GameRes://src/settings.js');
     BK.Script.loadlib();
@@ -187,12 +187,12 @@ if (false) {
 else if (window.jsb) {
     var isRuntime = (typeof loadRuntime === 'function');
     if (isRuntime) {
-        require('src/settings.ccb44.js');
+        require('src/settings.5d3f2.js');
         require('src/cocos2d-runtime.js');
         require('jsb-adapter/engine/index.js');
     }
     else {
-        require('src/settings.ccb44.js');
+        require('src/settings.5d3f2.js');
         require('src/cocos2d-jsb.js');
         require('jsb-adapter/jsb-engine.js');
     }
