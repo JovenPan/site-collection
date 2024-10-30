27e29284bc04606546ab3e4a4b7c1e7fc1baa311
JovenPan <joven@macbook-pro-15>
2024-10-31 01:07:33 +0800

    feat: 调试智商在哪儿devicemotion

--- cocos/unbelievable/wechat/main.c9cc6.js
+++ cocos/unbelievable/wechat/main.c9cc6.js
@@ -135,7 +135,7 @@ window.boot = function () {
         BK.Script.loadlib();
     }
     else {
-        var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.323e9.js';
+        var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.a3161.js';
         if (jsList) {
             jsList = jsList.map(function (x) {
                 return 'src/' + x;
@@ -161,7 +161,7 @@ window.boot = function () {
     cc.game.run(option, onStart);
 };
 
-// main.b049b.js is qqplay and jsb platform entry file, so we must leave platform init code here
+// main.c9cc6.js is qqplay and jsb platform entry file, so we must leave platform init code here
 if (false) {
     BK.Script.loadlib('GameRes://src/settings.js');
     BK.Script.loadlib();
