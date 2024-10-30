9e5345af8ca6d5ece1efb6128bba86e7bcb337e7
JovenPan <joven@macbook-pro-15>
2024-10-31 01:59:06 +0800

    feat: cocos unbelievable fix

--- cocos/unbelievable/facebook/main.a5a5f.js
+++ cocos/unbelievable/facebook/main.a5a5f.js
@@ -135,7 +135,7 @@ window.boot = function () {
         BK.Script.loadlib();
     }
     else {
-        var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.44b35.js';
+        var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.d03dc.js';
         if (jsList) {
             jsList = jsList.map(function (x) {
                 return 'src/' + x;
@@ -161,7 +161,7 @@ window.boot = function () {
     cc.game.run(option, onStart);
 };
 
-// main.9f76f.js is qqplay and jsb platform entry file, so we must leave platform init code here
+// main.a5a5f.js is qqplay and jsb platform entry file, so we must leave platform init code here
 if (false) {
     BK.Script.loadlib('GameRes://src/settings.js');
     BK.Script.loadlib();
@@ -187,12 +187,12 @@ if (false) {
 else if (window.jsb) {
     var isRuntime = (typeof loadRuntime === 'function');
     if (isRuntime) {
-        require('src/settings.99ce5.js');
+        require('src/settings.02142.js');
         require('src/cocos2d-runtime.js');
         require('jsb-adapter/engine/index.js');
     }
     else {
-        require('src/settings.99ce5.js');
+        require('src/settings.02142.js');
         require('src/cocos2d-jsb.js');
         require('jsb-adapter/jsb-engine.js');
     }
--- cocos/unbelievable/wechat/main.ad14a.js
+++ cocos/unbelievable/wechat/main.ad14a.js
@@ -135,7 +135,7 @@ window.boot = function () {
         BK.Script.loadlib();
     }
     else {
-        var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.3664f.js';
+        var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.4e924.js';
         if (jsList) {
             jsList = jsList.map(function (x) {
                 return 'src/' + x;
@@ -161,7 +161,7 @@ window.boot = function () {
     cc.game.run(option, onStart);
 };
 
-// main.8f323.js is qqplay and jsb platform entry file, so we must leave platform init code here
+// main.ad14a.js is qqplay and jsb platform entry file, so we must leave platform init code here
 if (false) {
     BK.Script.loadlib('GameRes://src/settings.js');
     BK.Script.loadlib();
@@ -187,12 +187,12 @@ if (false) {
 else if (window.jsb) {
     var isRuntime = (typeof loadRuntime === 'function');
     if (isRuntime) {
-        require('src/settings.3c429.js');
+        require('src/settings.99ce5.js');
         require('src/cocos2d-runtime.js');
         require('jsb-adapter/engine/index.js');
     }
     else {
-        require('src/settings.3c429.js');
+        require('src/settings.99ce5.js');
         require('src/cocos2d-jsb.js');
         require('jsb-adapter/jsb-engine.js');
     }
